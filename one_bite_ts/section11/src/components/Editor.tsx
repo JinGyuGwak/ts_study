d73d6dea7b0751a548ddcf0c0898b6962cf3bcf9
JinGyuGwak <wlsrb2695@naver.com>
2025-08-03 16:09:47 +0900

    타스 강의 완

---
+++ one_bite_ts/section11/src/components/Editor.tsx
@@ -0,0 +1,26 @@
+import { useContext, useState } from "react";
+import { TodoDispatchContext, useTodoDispatch } from "../App";
+
+interface Props {}
+
+export default function Editor(props: Props) {
+  const dispatch = useTodoDispatch();
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const [text, setText] = useState("");
+
+  const onClickButton = () => {
+    dispatch?.onClickAdd(text);
+    setText("");
+  };
+
+  return (
+    <div>
+      <input value={text} onChange={onChangeInput} />
+      <button onClick={onClickButton}>추가</button>
+    </div>
+  );
+}
